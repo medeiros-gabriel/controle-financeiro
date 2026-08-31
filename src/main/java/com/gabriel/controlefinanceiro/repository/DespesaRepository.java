c420fc4751a5429a73b849e6b64bbfecc72f8689
Gabriel Medeiros <gabriel.mdrs.br@gmail.com>
2026-08-31 14:10:22 -0300

    feat: cria DespesaRepository para operacoes no banco

---
+++ src/main/java/com/gabriel/controlefinanceiro/repository/DespesaRepository.java
@@ -0,0 +1,11 @@
+package com.gabriel.controlefinanceiro.repository;
+
+import com.gabriel.controlefinanceiro.model.Despesa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DespesaRepository extends JpaRepository<Despesa, Integer> {
+    // Ao herdar de JpaRepository<Despesa, Integer>, o Spring te dá
+    // automaticamente métodos como: save(), findAll(), findById(), deleteById(), etc.
+}
